1a8ae27f7df7b29d74e610cddf3a24e3469e4951
erp12 <erp12@hampshire.edu>
2015-02-10 23:24:15 -0500

    Added Gravity Sim

---
+++ Assets/Globals.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Globals : MonoBehaviour {
+	//public static float G = 0.00006673f; // actually 6.673×10^−11 N·(m/kg)^2
+	public static float G = 0.006673f;
+	public static int lastID;
+
+	public static int InitialPosMag = 10;	// in km from (0, 0, 0)
+	public static int InitialVelMag = 10;	// in km/frame
+	public static int MaxMass = 1000;		// in kg
+	private static int density = 2000;		// in kg/m^3	5510 = earth	1408 = sun
+
+	public GameObject planetPrefab;
+	public static List<GameObject> planets = new List<GameObject>();
+ 
+	public static int GetNextID() {
+		lastID++;
+		return lastID;
+	}
+
+	public static Vector3 MassToScale(float mass) {
+		float d = mass/density;
+		return new Vector3(d, d, d);
+	}
+
+	public static Color RandomDistinctColor() {
+		switch((int)Random.Range(0, 6)) {
+			case 0 : return Color.red;
+				break;
+			case 1 : return Color.blue;
+				break;
+			case 2 : return Color.green;
+				break;
+			case 3 : return Color.yellow;
+				break;
+			case 4 : return Color.cyan;
+				break;
+			case 5 : return Color.magenta;
+				break;
+			default : return Color.white;
+		}	
+	}
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnPlanet", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnPlanet(){
+		GameObject newPlanet = Instantiate(planetPrefab) as GameObject;
+		planets.Add(newPlanet);
+	}
+}
---
+++ Assets/Planet.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Planet : MonoBehaviour {
+	public GameObject pathMarker;
+	private Rigidbody rb;
+	private int ID;
+	private Color color;
+
+	// Use this for initialization
+	void Start () {
+		ID = Globals.GetNextID();
+		rb = GetComponent<Rigidbody>();
+		color = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f));
+		InvokeRepeating("DropPathPoint", 0.5f, 0.5f);
+		RandInit();
+	}
+
+	void RandInit() {
+		// Set random Mass
+		rb.mass = Random.Range(0, Globals.MaxMass);
+		transform.localScale = Globals.MassToScale(rb.mass);
+
+		// Set random initial position and velocity
+		transform.position = new Vector3(Random.Range(-Globals.InitialPosMag, Globals.InitialPosMag),
+		                                 Random.Range(-Globals.InitialPosMag, Globals.InitialPosMag),
+		                                 Random.Range(-Globals.InitialPosMag, Globals.InitialPosMag));
+		rb.velocity = new Vector3(Random.Range(-Globals.InitialVelMag, Globals.InitialVelMag),
+		                          Random.Range(-Globals.InitialVelMag, Globals.InitialVelMag),
+		                          Random.Range(-Globals.InitialVelMag, Globals.InitialVelMag));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject other in Globals.planets) {
+			int IDofOther = ((Planet)other.GetComponent(typeof(Planet))).GetID();
+			if (IDofOther != ID) {
+				float forceMagnitude = Globals.G * ((other.rigidbody.mass * rb.mass) / Vector3.Distance(other.transform.position, transform.position));
+				Vector3 forceVector = (other.transform.position - transform.position).normalized;
+				rigidbody.AddForce(forceVector * forceMagnitude);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals("Planet")) {
+			print("Planet Drestroyed with ID: " + ID);
+			Destroy(this.gameObject);
+		}
+	}
+
+	void DropPathPoint() {
+		GameObject pp = Instantiate(pathMarker, transform.position, Quaternion.identity) as GameObject;
+		pp.renderer.material = new Material (Shader.Find("Self-Illumin/Diffuse"));
+		pp.renderer.material.color = color;
+	}
+
+	int GetID() {
+		return ID;
+	}
+}
